9280996a4e8b9021d9a49ddf2e286d9be6beeab6
Nitish Kulkarni <nkulkarni@nitishs-macbook.lan>
2020-03-27 23:41:32 -0700

    making schema optional, adding multiple transformation steps

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -5,10 +5,14 @@ from src.dataset import PandasDataset
 class TestDataset(TestCase):
 
 	def test_simple_transformation(self):
-		pandas_dataset = PandasDataset("resources/test.csv", None, None)
+		pandas_dataset = PandasDataset("resources/test.csv", None, None).load()
+		pandas_dataset.transform(lambda x: x['a'] == 4)
+		pandas_dataset.compute()
+		self.assertEqual(len(pandas_dataset.data.index), 2)
 
-		pandas_dataset = pandas_dataset.transform(lambda x: x['a'] == 4)
-		pandas_dataset = pandas_dataset.compute()
-
-		assert len(pandas_dataset.data.index) == 1
-
+	def test_multi_stage_transformations(self):
+		pandas_dataset = PandasDataset("resources/test.csv", None, None).load()
+		pandas_dataset.transform(lambda x: x['a'] == 4)
+		pandas_dataset.transform(lambda x: x['b'] > 5)
+		pandas_dataset.compute()
+		self.assertEqual(len(pandas_dataset.data.index), 1)
